9ec4d87fc9c56291f02c846e659c28e230ddeb5d
Rayenbabdallah <rayenbenabdallah88@gmail.com>
2026-04-04 11:59:22 +0100

    update StudyTask

--- app/src/main/java/com/example/studypartner/StudyTask.kt
+++ app/src/main/java/com/example/studypartner/StudyTask.kt
@@ -1,6 +1,11 @@
 package com.example.studypartner
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
 data class StudyTask(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val subject: String,
     val difficulty: Int,
