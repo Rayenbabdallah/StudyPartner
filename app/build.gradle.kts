946caa14ff14ee4c9bd7f8ea23e0d7239dbe5e75
Rayenbabdallah <rayenbenabdallah88@gmail.com>
2026-04-04 15:39:22 +0100

    add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
